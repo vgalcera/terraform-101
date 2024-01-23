0c33ebab4e5be29bcfa625deb554d0babf3e5c60
Vanessa <vgalcera@gmail.com>
2024-01-23 13:41:48 +0100

    Feat: bucket object added with html file

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ resource "aws_s3_bucket" "example_bucket" {
        Project = "Terraform-101"
     }
 }
+
+resource "aws_s3_object" "bucket_object" {
+    bucket = aws_s3_bucket.example_bucket.id
+    key = "bucket-obj-hello-world"
+    source = "hello-world.html"
+    content_type = "text/html"
+}
